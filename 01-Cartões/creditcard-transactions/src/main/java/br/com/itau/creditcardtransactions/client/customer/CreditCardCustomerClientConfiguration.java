28ae0900a18def9c48c9a087927b00993b979e5e
Jefferson Brito <jefferson.brit@gmail.com>
2020-08-07 16:09:29 -0300

    no message

--- 01-Cartões/creditcard-transactions/src/main/java/br/com/itau/creditcardtransactions/client/customer/CreditCardCustomerClientConfiguration.java
+++ 01-Cartões/creditcard-transactions/src/main/java/br/com/itau/creditcardtransactions/client/customer/CreditCardCustomerClientConfiguration.java
@@ -1,12 +1,27 @@
 package br.com.itau.creditcardtransactions.client.customer;
 
-import br.com.itau.creditcardtransactions.client.OAuth2FeignConfiguration;
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 
-public class CreditCardCustomerClientConfiguration extends OAuth2FeignConfiguration {
+public class CreditCardCustomerClientConfiguration {
     @Bean
     public ErrorDecoder getErrorDecoder() {
         return new CreditCardCustomerClientDecoder();
+    }
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
+
+    @Autowired
+    private ClientCredentialsResourceDetails clientCredentialsResourceDetails;
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor() {
+        return new OAuth2FeignRequestInterceptor(oAuth2ClientContext, clientCredentialsResourceDetails);
     }
 }
--- 01-Cartões/creditcard-transactions/src/main/java/br/com/itau/creditcardtransactions/client/payment/PaymentsClientConfiguration.java
+++ 01-Cartões/creditcard-transactions/src/main/java/br/com/itau/creditcardtransactions/client/payment/PaymentsClientConfiguration.java
@@ -1,12 +1,27 @@
 package br.com.itau.creditcardtransactions.client.payment;
 
-import br.com.itau.creditcardtransactions.client.OAuth2FeignConfiguration;
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 
-public class PaymentsClientConfiguration extends OAuth2FeignConfiguration {
+public class PaymentsClientConfiguration {
     @Bean
     public ErrorDecoder getErrorDecoder() {
         return new PaymentsClientDecoder();
+    }
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
+
+    @Autowired
+    private ClientCredentialsResourceDetails clientCredentialsResourceDetails;
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor() {
+        return new OAuth2FeignRequestInterceptor(oAuth2ClientContext, clientCredentialsResourceDetails);
     }
 }
